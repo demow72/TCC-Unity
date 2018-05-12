02147ae1f2f25ecc367522397db6f8e857342708
William Ferreira <demow72@gmail.com>
2018-05-12 14:44:10 -0300

    Criação do controlador do jogo e função pra aparecer inimigo incompleta

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance = null;
+
+	public GameObject spawnPoint; // lugar onde os inimigos irão aparecer
+	public GameObject[] enemies; //array pra 3 tipos de inimigos 
+	public int maxEnemiesOnScreen; // nr de inimigos na tela, váriavel criada pra manipular a dificuldade
+	public int totalEnemies;
+	public int enemiesPerSpawn; // Quantos enemigos vão ser gerados por spawn
+
+	private int enemiesOnScreen = 0; //Então toda a vez q spawnarmos inimigos incrementaremos com essa váriavel
+	// Use this for initialization
+	void Awake(){
+		if (instance == null)   // Se a instance for igual a null nos queremos setar a intancia a ela mesma
+			instance = this;
+		else if (instance != this) // Se n for igual a ela mesma queremos destruir o gameObjetct, 
+			Destroy(gameObject);
+		
+		DontDestroyOnLoad (gameObject);
+	}
+
+	void Start () {
+		spawnEnemy ();
+	}
+
+	void spawnEnemy(){
+		if ( enemiesPerSpawn > 0 && enemiesOnScreen < totalEnemies) { // Se enimigosporSpawn for menor que 0 E os inimigos na tela forem menores que o nr total de enimigos
+			for (int i=0; i <enemiesPerSpawn; i++){                   // i = 0 enquanto i for menor que inimigos por spawn SEGUE O BAILE
+				if (enemiesOnScreen < maxEnemiesOnScreen) {           // Se os inimigos na tela forem MENOR que o nr MAX de inimigos na tela, instancia o gameObjetct e transoforma a posição dele mostrando os inimigo
+					GameObject newEnemy = Instantiate(enemies[0]) as GameObject;
+						newEnemy.transform.position = spawnPoint.transform.position;
+						enemiesOnScreen = enemiesOnScreen +1 ;
+				}
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Loader.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loader : MonoBehaviour {
+
+	// loader criado para conectar o script a camera principal, pq ela sempre da load
+	//Quando rodamos a main camera será chamada e instanciaremos o jogo uma vez
+
+	public GameObject gameManager; // Tem que ser gameObject, pq quando criamos o GameManager nós criamos um gameobject vázio
+
+	void Awake () {								//Vai ser chamado quando carregar o jogo
+		if (GameManager.instance == null)				//checa se o gameManager é null
+			Instantiate (gameManager);				//instancia o gameManager
+	}
+}
